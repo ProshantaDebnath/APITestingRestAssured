89d8b0a27e972a762bd72fd7233d8d004748039c
Proshanta Debnath <debnathproshanta80@gmail.com>
2026-02-10 00:31:17 +0530

    AuthService Completed for Postrequest

--- src/test/java/com/api/tests/LogInAPITest.java
+++ src/test/java/com/api/tests/LogInAPITest.java
@@ -1,26 +1,27 @@
 package com.api.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
+import com.api.base.AuthService;
+import com.api.models.request.LoginRequest;
+
 import io.restassured.response.Response;
-import static io.restassured.RestAssured.*;
+
 
 
 public class LogInAPITest {
 
 	@Test(description = "Verify is api is working or not")
 	public void loginTest() {
+	
+	//creating the pojo object then pass it over AuthServices
+	LoginRequest loginRequest = new LoginRequest("","");
 
-		Response response = given()
-							.baseUri("http://64.227.160.186:8080")
-							.header("Content-Type", "application/json")
-							.body("{\"username\": \"\",\"password\": \"\"}")
-							.post("/api/auth/login");
-		
-		Assert.assertEquals(response.getStatusCode(), 200);
-		
-		System.out.println(response.asPrettyString());
-
+	AuthService authservice = new AuthService();
+	
+	Response response = authservice.login(loginRequest);
+	
+	System.out.println(response.asPrettyString());
+	
 	}
 
 }
